c46a437c00a7e0fa4134e55677dc43bb78d14d91
ninokhh <nino.isoiani1@gmail.com>
2026-01-12 21:38:59 +0400

    edit

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import os
 from app import app
+from extensions import db
+from app import ensure_default_admin
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        ensure_default_admin()
+
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
